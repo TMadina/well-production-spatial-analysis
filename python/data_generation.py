fad319a3ba5a0a49c79514ef15f57f3385139b8e
Madina <tmadina1411@gmail.com>
2026-03-24 22:15:51 +0500

    Add files via upload

--- python/data_generation.py
+++ python/data_generation.py
@@ -1,1 +1,26 @@
+import random
 
+layer = iface.activeLayer()
+
+
+from qgis.PyQt.QtCore import QVariant
+
+provider = layer.dataProvider()
+
+provider.addAttributes([
+    QgsField("depth_m", QVariant.Int),
+    QgsField("production", QVariant.Double)])
+layer.updateFields()
+
+
+with edit(layer):
+    for f in layer.getFeatures():
+        depth = random.randint(700, 1500)
+        
+
+        production = depth * random.uniform(0.05, 0.2)
+        
+        f["depth_m"] = depth
+        f["production"] = production
+        
+        layer.updateFeature(f)
